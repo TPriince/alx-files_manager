63c8907cd9e5b07ef0b75383006a2efc0c46eed5
TPriince <tobiloba110@gmail.com>
2023-03-04 18:12:26 -0800

    Task 0 - Redis utils

--- utils/redis.js
+++ utils/redis.js
@@ -1,6 +1,12 @@
 import { createClient } from 'redis';
 
+/**
+ * Represents a Redis client.
+ */
 class RedisClient {
+	/**
+	 * Creates a new RedisClient instance.
+	 */
 	constructor() {
 		this.client = createClient();
 		this.isConnected = true;
@@ -12,11 +18,20 @@ class RedisClient {
 			this.isConnected = true;
 		});
 	}
-	
+
+	/**
+	 * Checks if the connection to Redis is a success or not
+	 * @returns {boolean}
+	 */
 	isAlive() {
 		return this.isConnected;
 	}
 
+	/**
+	 * Retrieves the value of a given key.
+	 * @param {String} key The key of the item to retrieve.
+	 * @returns {String | Object}
+	 */
 	async get(key) {
 		return new Promise((resolve, reject) => {
 			this.client.get(key, (err, val) => {
@@ -38,6 +53,13 @@ class RedisClient {
 		});
 	}
 
+	/**
+	 * Stores a key and its value along with an expiration time.
+	 * @param {String} key The key of the item to store.
+	 * @param {String | Number | Boolean} value The item to store.
+	 * @param {Number} duration The expiration time of the item in seconds.
+	 * @returns {Promise<void>}
+	 */
 	async set(key, value, duration) {
 		return new Promise((resolve) => {
 			this.client.set(key, value, 'EX', duration, function(err, reply) {
@@ -50,9 +72,14 @@ class RedisClient {
 		})
 	}
 
+	/**
+	 * Removes the value of a given key.
+	 * @param {String} key The key of the item to remove.
+	 * @returns {Promise<void>}
+	 */
 	async del(key) {
 		return new Promise((resolve, reject) => {
-			client.del(key, (err, reply) => {
+			this.client.del(key, (err, reply) => {
 				if (err) {
 					return reject(err);
 				}
